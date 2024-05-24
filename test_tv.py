bad0867d45be880d12e859958bdf00ac0cc9362b
unknown <mangulabnanshervinmarco@gmail.com>
2024-05-24 17:15:22 +0700

    turn on the TVs

--- test_tv.py
+++ test_tv.py
@@ -1,8 +1,14 @@
 # Pseudocode
-# Create two TV objects
-tv1 = TV()
-tv2 = TV()
-# Turn on the TVs
-# Set channels and volumes for tv1
-# Set channels and volumes for tv2
-# Print the results
+
+from tv_class import TV
+
+def main():
+    # Create two TV objects
+    tv1 = TV()
+    tv2 = TV()
+    # Turn on the TVs
+    tv1.turnOn()
+    tv2.turnOn()
+    # Set channels and volumes for tv1
+    # Set channels and volumes for tv2
+    # Print the results

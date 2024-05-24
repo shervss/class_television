9f6f26d6500b97abd8cce7ea648c620aab062b51
unknown <mangulabnanshervinmarco@gmail.com>
2024-05-24 17:08:44 +0700

    fix some errors

--- tv_class.py
+++ tv_class.py
@@ -2,11 +2,11 @@
 
 class TV:
     def __init__(self):
-    # Construct a default TV object
-    # Initialize the TV with channel 1, volume level 1, and turned off
-    self.channel = 1
-    self.volumeLevel = 1
-    self.on = False
+        # Construct a default TV object
+        # Initialize the TV with channel 1, volume level 1, and turned off
+        self.channel = 1
+        self.volumeLevel = 1
+        self.on = False
     # Turn on the TV
     def turn_on(self):
         self.on = True
@@ -22,7 +22,7 @@ class TV:
             self.channel = channel
     # Return the current volume level of the TV
     def get_volume(self):
-        ruturn self.volumeLevel
+        return self.volumeLevel
     # Set a new volume level for the TV if the TV is on and the volume level is within the range (1 to 7)
     def set_volume(self,volumeLevel):
         if 1 <= volumeLevel <= 7:
@@ -38,6 +38,7 @@ class TV:
     # Increase the volume level by 1 if the TV is on and the current volume level is less than 7
     def volume_up(self):
         if self.on and self.volumeLevel < 7:
+            self.volumeLevel += 1
     # Decrease the volume level by 1 if the TV is on and the current volume level is greater than 1
     def volume_down(self):
         if self.on and self.volumeLevel > 1:

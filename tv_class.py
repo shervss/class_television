f10cdfff7c8bb1ea4f1693705bca219583b67fe7
unknown <mangulabnanshervinmarco@gmail.com>
2024-05-24 12:13:36 +0700

    # Initialize the TV with channel 1, volume level 1, and turned off

--- tv_class.py
+++ tv_class.py
@@ -1,13 +1,19 @@
 # Pseudocode
-# Construct a default TV object
-# Initialize the TV with channel 1, volume level 1, and turned off
-# Turn on the TV
-# Turn off the TV
-# Return the channel of the current TV
-# Set a new channel for the TV if the TV is on and the channel is within the range (1 to 120)
-# Return the current volume level of the TV
-# Set a new volume level for the TV if the TV is on and the volume level is within the range (1 to 7)
-# Increase the channel number by 1 if the TV is on and the current channel is less than 120
-# Decrease the channel number by 1 if the TV is on and the current channel is greater than 1
-# Increase the volume level by 1 if the TV is on and the current volume level is less than 7
-# Decrease the volume level by 1 if the TV is on and the current volume level is greater than 1
+
+class TV:
+    def __init__(self):
+    # Construct a default TV object
+    # Initialize the TV with channel 1, volume level 1, and turned off
+    self.channel = 1
+    self.volumeLevel = 1
+    self.on = False
+    # Turn on the TV
+    # Turn off the TV
+    # Return the channel of the current TV
+    # Set a new channel for the TV if the TV is on and the channel is within the range (1 to 120)
+    # Return the current volume level of the TV
+    # Set a new volume level for the TV if the TV is on and the volume level is within the range (1 to 7)
+    # Increase the channel number by 1 if the TV is on and the current channel is less than 120
+    # Decrease the channel number by 1 if the TV is on and the current channel is greater than 1
+    # Increase the volume level by 1 if the TV is on and the current volume level is less than 7
+    # Decrease the volume level by 1 if the TV is on and the current volume level is greater than 1

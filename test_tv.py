81e8a40b3b67d69a334ab331fdd3667acc76c992
unknown <mangulabnanshervinmarco@gmail.com>
2024-05-24 17:18:17 +0700

    set channels and volumes for tv1

--- test_tv.py
+++ test_tv.py
@@ -7,8 +7,10 @@ def main():
     tv1 = TV()
     tv2 = TV()
     # Turn on the TVs
-    tv1.turnOn()
-    tv2.turnOn()
+    tv1.turn_on()
+    tv2.turn_on()
     # Set channels and volumes for tv1
+    tv1.set_channel(30)
+    tv1.set_volume(3)
     # Set channels and volumes for tv2
     # Print the results
